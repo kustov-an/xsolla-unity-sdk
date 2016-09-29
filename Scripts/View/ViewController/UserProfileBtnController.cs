de1d405d068af98d047c1352fc938f311bb33dfc
p.yurkin <p.yurkin@xsolla.com>
2016-09-29 16:44:12 +0500

    -\\-

--- Scripts/View/ViewController/UserProfileBtnController.cs
+++ Scripts/View/ViewController/UserProfileBtnController.cs
@@ -1,9 +1,20 @@
 ﻿using System;
+using UnityEngine.UI;
+using UnityEngine;
 
-namespace AssemblyCSharp
+namespace Xsolla
 {
-	public class UserProfileBtnController
+	public class UserProfileBtnController: MonoBehaviour
 	{
+		public Text _title;
+		public Button _btn;
+
+		public void InitScreen(String pName, Action pAction)
+		{
+			_title.text = pName;
+			_btn.onClick.AddListener(delegate {pAction();});
+		}
+
 		public UserProfileBtnController ()
 		{
 		}
--- Scripts/View/ViewController/UserProfileController.cs
+++ Scripts/View/ViewController/UserProfileController.cs
@@ -1,13 +1,24 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Xsolla 
 {
-	public class UserProfileController
+	public class UserProfileController: MonoBehaviour
 	{
+
+		private List<UserProfileBtnController> listBtn;
+
 		public UserProfileController ()
 		{
+			listBtn = new List<UserProfileBtnController>();
+		}
+
+		public void AddBtn(UserProfileBtnController pBtn)
+		{
+			listBtn.Add(pBtn);
 		}
 	}
 }
